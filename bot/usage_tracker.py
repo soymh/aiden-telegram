f3e6f5b38fe2022b4af75145eb5cc98a5c67622f
MH <176279314+soymh@users.noreply.github.com>
2025-04-12 13:42:12 +0330

    bug fix

--- bot/usage_tracker.py
+++ bot/usage_tracker.py
@@ -225,23 +225,21 @@ class UsageTracker:
         now = datetime.now()
         formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
 
-        conversation = self.usage['conversations']
-
         if update_value:
             if not reset:
-                if str(chat_id) in conversation :
-                    conversation[str(chat_id)].append(update_value)
+                if str(chat_id) in self.usage['conversations'] :
+                    self.usage['conversations'][str(chat_id)].append(update_value)
                 else :
-                    conversation[str(chat_id)] = []
-                    conversation[str(chat_id)].append(update_value)
+                    self.usage['conversations'][str(chat_id)] = []
+                    self.usage['conversations'][str(chat_id)].append(update_value)
             else:
-                conversation[str(formatted_now)] = conversation[str(chat_id)]
-                conversation[str(chat_id)] = [update_value]
+                self.usage['conversations'][str(formatted_now)] = self.usage['conversations'][str(chat_id)]
+                self.usage['conversations'][str(chat_id)] = [update_value]
             with open(self.user_file, "w") as outfile:
                 json.dump(self.usage, outfile, indent=4)
             return
         
-        main_conversation = conversation[str(chat_id)]
+        main_conversation = self.usage['conversations'][str(chat_id)]
         main_conversation = {chat_id: main_conversation}
 
         return main_conversation
